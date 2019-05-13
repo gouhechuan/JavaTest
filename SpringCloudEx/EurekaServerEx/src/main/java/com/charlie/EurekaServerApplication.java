64bfc6e73d7e0135d1a874eb43c72a58059f1fd3
charlie.gou <charlie.gou@connext.com.cn>
2019-05-13 21:56:51 +0800

    1

--- SpringCloudEx/EurekaServerEx/src/main/java/com/charlie/EurekaServerApplication.java
+++ SpringCloudEx/EurekaServerEx/src/main/java/com/charlie/EurekaServerApplication.java
@@ -2,11 +2,13 @@ package com.charlie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
 @EnableEurekaServer
 @SpringBootApplication
+//@EnableConfigServer
 public class EurekaServerApplication {
 
     public static void main(String[] args){
---
+++ SpringCloudEx/configServerEx/src/main/java/com/charlie/ConfigServerApplication.java
@@ -0,0 +1,28 @@
+package com.charlie;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableConfigServer
+@SpringBootApplication
+@EnableEurekaServer
+@RestController
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+    @RequestMapping("/")
+    public String home() {
+        return "config Server from port " + port;
+    }
+}
